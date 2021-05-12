6cf51a2cee7c2738289a9c1de90d77c03f43a272
Diego Araujo <diego_shakan@hotmail.com>
2021-05-12 06:22:49 -0300

    test(app): create third test to maximize_array

--- spec/algorithms/greddy/maximize_array_spec.rb
+++ spec/algorithms/greddy/maximize_array_spec.rb
@@ -12,12 +12,21 @@ RSpec.describe "Maximize Array" do
     expect(result).to eq(10)
   end
 
-  it "Return Sum from Maximize Array 1" do
+  it "Return Sum from Maximize Array 2" do
     arr = [9, 8, 8, 5]
     k = 3
 
     result = Algorithms::Greddy::MaximizeArray.new.run(arr, k)
 
     expect(result).to eq(20)
+  end
+
+  it "Return Sum from Maximize Array 3" do
+    arr = [-3, -2, -1, 5, 6]
+    k = 4
+
+    result = Algorithms::Greddy::MaximizeArray.new.run(arr, k)
+
+    expect(result).to eq(15)
   end
 end
