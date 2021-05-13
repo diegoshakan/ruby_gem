60692e5951f2b1e1f5ae4012ae2487b699e0afe4
Diego Araujo <diego_shakan@hotmail.com>
2021-05-13 06:18:48 -0300

    refac(binary_search): refact to binary search

--- lib/ruby_gem/algorithms/searching/binary_search.rb
+++ lib/ruby_gem/algorithms/searching/binary_search.rb
@@ -1,22 +1,30 @@
-def binary_search(value, list, first, last)
-  mid = ((first + last) / 2).floor
+# frozen_string_literal: true
 
-  if first <= last
-    if value > list[mid]
-      first = mid + 1
-      binary_search(value, list, first, last)
-    elsif value < list[mid]
-      last = mid - 1
-      binary_search(value, list, first, last)
-    else
-      mid
+module Algorithms
+  module Searching
+    class BinarySearch
+      def run(value, list, first, last)
+        mid = ((first + last) / 2).floor
+
+        if first <= last
+          if value > list[mid]
+            first = mid + 1
+            Algorithms::Searching::BinarySearch.new.run(value, list, first, last)
+          elsif value < list[mid]
+            last = mid - 1
+            Algorithms::Searching::BinarySearch.new.run(value, list, first, last)
+          else
+            mid
+          end
+        else
+          -1
+        end
+      end
     end
-  else
-    -1
   end
 end
 
-list = [12, 15, 19, 22, 25, 65, 98, 102, 325]
-
-result = binary_search(19, list, 0, list.size)
-puts result
+# list = [12, 15, 19, 22, 25, 65, 98, 102, 325]
+#
+# result = binary_search(19, list, 0, list.size)
+# puts result
